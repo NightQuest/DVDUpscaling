d829be5e29a20cb76886a3095f16698c2f8ca61c
Amelia <NightQuest@users.noreply.github.com>
2023-09-24 21:00:34 -0700

    Use CRF 24

--- Upscale.ps1
+++ Upscale.ps1
@@ -403,7 +403,7 @@ ForEach ($file in $files)
         $frameRate = $UpscaledMediaFile.getAttribute("Video", "FrameRate")
 
         # Encode with x265
-        Start-Process -FilePath "cmd" -ArgumentList "/c `"`"$ffmpeg_path`" -y -hide_banner -loglevel error -f mov -i `"$upscale_path`" -strict -1 -f yuv4mpegpipe - | `"$x265_path`" --log-level none --y4m --input - --input-res $($config.upscale_Width)x$($config.upscale_Height) --fps $frameRate --frames $frameCount --input-depth 10 --profile main422-10 --level-idc 5.2 --preset placebo --tune grain --crf 20 --rd 4 --psy-rd 0.75 --psy-rdoq 4.0 --rdoq-level 1 --no-strong-intra-smoothing --aq-mode 1 --rskip 2 --no-rect --output `"$encode_path`"`""  -NoNewWindow -Wait
+        Start-Process -FilePath "cmd" -ArgumentList "/c `"`"$ffmpeg_path`" -y -hide_banner -loglevel error -f mov -i `"$upscale_path`" -strict -1 -f yuv4mpegpipe - | `"$x265_path`" --log-level none --y4m --input - --input-res $($config.upscale_Width)x$($config.upscale_Height) --fps $frameRate --frames $frameCount --input-depth 10 --profile main422-10 --level-idc 5.2 --preset placebo --tune grain --crf 24 --rd 4 --psy-rd 0.75 --psy-rdoq 4.0 --rdoq-level 1 --no-strong-intra-smoothing --aq-mode 1 --rskip 2 --no-rect --output `"$encode_path`"`""  -NoNewWindow -Wait
 
         if (Test-Path -LiteralPath $encode_path -PathType 'Leaf')
         {
